2112a7b0db4d90fe6e4858211199066d1bfd55c6
Parkayun <iamparkayun@gmail.com>
2015-11-29 17:58:57 +0900

    Improved search api

--- app.py
+++ app.py
@@ -6,19 +6,18 @@ from astrid import Astrid
 from astrid.http import response
 
 
-YOUTUBE = build('youtube', 'v3', developerKey="")
+YOUTUBE = build('youtube', 'v3', developerKey="AIzaSyCWumvIWyysRd5VsKCrcdF2Z5Fak3oV9fU")
 
 app = Astrid()
 
 
 @app.route('/api/search/{name}')
 def api_search(request):
+    result = {'status': 'success', 'data': None}
     query = request.match_info['name']
 
-    result = YOUTUBE.search().list(
-        q=query,
-        part="id,snippet",
-        maxResults=9
+    result['data'] = YOUTUBE.search().list(
+        q=query, part="id,snippet", maxResults=9
     ).execute()['items']
 
     return response(json.dumps(result))
